5ebcbc1e9a2b3c4ef3c84f093da478ada3d756ec
Christian-Enrique Hindremäe <90316663+CrimsonTLU@users.noreply.github.com>
2022-04-08 12:57:47 +0300

    Update team.js

--- team.js
+++ team.js
@@ -1,11 +1,6 @@
 let firstMember = $('img.active').attr('src');
 
-$('#next').click(function(){
+$('#nextMember button').click(function(){
     let currentMember = $('img.active');
     let nextMember = $('img.active').html('<img src="' + currentMember.attr('src') + 2 + '">').hide().fadeIn(2000, "linear").slideUp(1000).slideDown(1000);
-
-    currentMember.removeClass('active');
-    currentMember.addClass('hidden');
-    nextMember.removeClass('hidden');
-    nextMember.addClass('active');
 })
