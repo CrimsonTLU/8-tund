750e8da83793f9d39cb9f4d4bafe4f4947ae8486
Christian-Enrique Hindremäe <90316663+CrimsonTLU@users.noreply.github.com>
2022-04-08 15:06:06 +0300

    Update team.js

--- team.js
+++ team.js
@@ -1,6 +1,61 @@
-let firstMember = $('img.active').attr('src');
+let firstMember = document.querySelector("#activePic");
+let memberName = document.querySelector("#memberName");
+let memberBio = document.querySelector("#memberBio");
 
-$('#nextMember button').click(function(){
-    let currentMember = $('img.active');
-    let nextMember = $('img.active').html('<img src="' + currentMember.attr('src') + 2 + '">').hide().fadeIn(2000, "linear").slideUp(1000).slideDown(1000);
-})
+let picNum = 1;
+
+console.log(firstMember);
+console.log(memberName);
+console.log(memberBio);
+
+document.querySelector('#next').addEventListener('click', (function(){
+    if(picNum < 4){
+        picNum += 1;
+        firstMember.src = 'pics/liige1' + picNum + '.png';
+    }
+    else{
+        firstMember.src = 'pics/liige1' + picNum + '.png';
+        picNum = 1;
+    }
+    if(picNum == 1){
+        firstMember.src = 'pics/liige1' + picNum + '.png';
+    }
+    console.log(picNum);
+
+    if(picNum==1){
+        memberName.innerHTML = '<p id="memberName">May Denless</p>'
+    }
+    if(picNum==2){
+        memberName.innerHTML = '<p id="memberName">Mike Oxlong</p>'
+    }
+    if(picNum==3){
+        memberName.innerHTML = '<p id="memberName">Ben Dover</p>'
+    }
+    if(picNum==4){
+        memberName.innerHTML = '<p id="memberName">John Doe</p>'
+    }
+}))
+
+document.querySelector('#previous').addEventListener('click', (function(){
+    if(picNum > 1){
+        picNum -= 1;
+        firstMember.src = 'pics/liige1' + picNum + '.png';
+    } else {
+        picNum = 4;
+        firstMember.src = 'pics/liige1' + picNum + '.png';
+    }
+    console.log(picNum);
+
+    if(picNum==1){
+        memberName.innerHTML = '<p id="memberName">May Denless</p>'
+    }
+    if(picNum==2){
+        memberName.innerHTML = '<p id="memberName">Mike Oxlong</p>'
+    }
+    if(picNum==3){
+        memberName.innerHTML = '<p id="memberName">Ben Dover</p>'
+    }
+    if(picNum==4){
+        memberName.innerHTML = '<p id="memberName">John Doe</p>'
+    }
+}))
